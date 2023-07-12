97412696b122adf9de46cc1bb5d6d4bb9a5d7502
SalHyeok <122508452+SalHyeok@users.noreply.github.com>
2023-07-12 22:10:01 +0900

    [Bronze V] Title: Rook, Time: 72 ms, Memory: 11268 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+	
+    public static void main(String[] args) throws NumberFormatException, IOException {    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//    	Scanner sc = new Scanner(System.in);
+    	System.out.println("  ___  ___  ___\n"
+    			+ "  | |__| |__| |\n"
+    			+ "  |           |\n"
+    			+ "   \\_________/\n"
+    			+ "    \\_______/\n"
+    			+ "     |     |\n"
+    			+ "     |     |\n"
+    			+ "     |     |\n"
+    			+ "     |     |\n"
+    			+ "     |_____|\n"
+    			+ "  __/       \\__\n"
+    			+ " /             \\\n"
+    			+ "/_______________\\");
+    	
+    	
+    }
+   
+
+}
