dd2a0eff3a80e8dbdabed2d1e54aaf25d2b4a958
SalHyeok <122508452+SalHyeok@users.noreply.github.com>
2023-09-01 21:04:37 +0900

    [Bronze V] Title: Pyramids, Time: 112 ms, Memory: 12816 KB -BaekjoonHub

---
+++ 백준/Bronze/5341. Pyramids/Pyramids.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+    static int[] dr = {-1, 0, 0, 1};
+    static int[] dc = {0, -1, 1, 0};
+
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true) {
+        	int n =sc.nextInt();
+        	if(n==0)break;
+        	System.out.println((n+1)*n/2);
+        }
+        
+    }
+}
