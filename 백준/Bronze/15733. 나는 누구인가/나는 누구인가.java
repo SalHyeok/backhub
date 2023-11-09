6d9413c10626924871abe1203a2c532a2cbdab76
SalHyeok <122508452+SalHyeok@users.noreply.github.com>
2023-11-09 21:55:14 +0900

    [Bronze V] Title: 나는 누구인가, Time: 104 ms, Memory: 12700 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);      
+        
+        System.out.println("I'm Sexy"); // 결과 출력
+    }
+}
