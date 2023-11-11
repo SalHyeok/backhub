ceeda694f8ead10876edcb481d16f2de5055684e
SalHyeok <122508452+SalHyeok@users.noreply.github.com>
2023-11-11 14:50:01 +0900

    [Bronze V] Title: 새로운 시작, Time: 100 ms, Memory: 12672 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);      
+        
+        System.out.println("파이팅!!"); // 결과 출력
+    }
+}
