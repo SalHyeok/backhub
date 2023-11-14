484818d8079f3ee02a232611e099e1a2ff5a1ae7
SalHyeok <122508452+SalHyeok@users.noreply.github.com>
2023-11-14 18:24:16 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.c
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
+long long int calculate(int A, int B) {
+    return (long long int)(A + B) * (long long int)(A - B);
+}
+
 int main() {
     int A, B;
     scanf("%d %d", &A, &B);
 
-    int result = (A + B) * (A - B);
-    printf("%d\n", result);
+    long long int result = calculate(A, B);
+    printf("%lld\n", result);
 
     return 0;
 }
