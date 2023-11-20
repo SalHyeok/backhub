779bf23ee8ff6d9fa8d7c9faa69affea855d17a2
SalHyeok <122508452+SalHyeok@users.noreply.github.com>
2023-11-21 00:24:52 +0900

    [Bronze V] Title: Site Score, Time: 112 ms, Memory: 12924 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int UR = sc.nextInt();
+        int TR = sc.nextInt();
+        int UO = sc.nextInt();
+        int TO = sc.nextInt();
+
+        int siteScore = (56 * UR) + (24 * TR) + (14 * UO) + (6 * TO);
+        System.out.println(siteScore);
+
+        sc.close();
+    }
+}
